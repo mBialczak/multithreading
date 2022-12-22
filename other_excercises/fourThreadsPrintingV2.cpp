175428c6f63886d486353ce5fb0f35cf4cda2d90
Maciej Białczak <m.bialczak@protonmail.com>
2022-12-22 20:12:07 +0100

    Minimal progress 22.12.2022

---
+++ other_excercises/fourThreadsPrintingV2.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+using namespace std::literals::chrono_literals;
+
+void printThreadId(std::mutex& mtx)
+{
+    std::lock_guard lock(mtx);
+    std::cout << std::this_thread::get_id() << "\n";
+}
+
+template <typename Duration>
+void printThread(int repetitions, Duration duration, std::mutex& mtx)
+{
+    for (int i = 0; i < repetitions; ++i) {
+        printThreadId(mtx);
+        std::this_thread::sleep_for(duration);
+    }
+}
+
+int main()
+{
+    constexpr int threadsNumber { 4 };
+    constexpr int repetitions { 12 };
+    constexpr auto duration = 2s;
+    std::mutex coutMtx;
+    std::vector<std::thread> threads;
+    for (int i = 0; i < threadsNumber; ++i) {
+        threads.emplace_back(printThread<decltype(duration)>, repetitions, duration, std::ref(coutMtx));
+    }
+
+    for (auto&& th : threads) {
+        th.join();
+    }
+}
